19f4723bd0cf0329731a4451ffaec1416a0f35cc
Shivam Yadav <yadavshivam7463@gmail.com>
2024-05-01 11:58:19 +0530

    Changes in sql queries files

--- Query-29th.sql
+++ Query-29th.sql
@@ -1,6 +1,28 @@
---Total Revenue
+
+
 
 USE [PowerBI-Pizza];
 
-select  sum(total_price) as Total_Revenue
+select top 5 * from pizza_sales ;
+
+------KPI-------------
+--Total Revenue-------
+select	sum(total_price) as Total_Revenue
+from dbo.pizza_sales;
+
+-- Average_Order_Value-------
+select sum(total_price)/Count(distinct order_id) as Average_Order_value
+from dbo.pizza_sales;
+
+---Total Pizza Sold--------
+select sum(quantity) as Total_Pizza_sold
+from dbo.pizza_sales;
+
+
+---Total Orders Placed-----
+select count(distinct order_id) as Total_Orders
+from dbo.pizza_sales;
+
+---Average Pizzas Sold----
+select sum(quantity)/count(distinct order_id) as Average_Pizza_Sold
 from dbo.pizza_sales;
